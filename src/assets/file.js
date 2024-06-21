e947c2573b26c0280e0d12d4313d8bb9749be962
ENSI Junior Entreprise <148913750+ENSIJuniorEntreprise@users.noreply.github.com>
2024-06-21 21:56:12 +0100

    Add files via upload

---
+++ src/assets/file.js
@@ -0,0 +1,23 @@
+import bannerImg from "./ENSIJE.jpg";
+import downloadIcon from "./downloadIcon.png";
+import mailIcon from "./mailIcon.png";
+import bannerrImg from "./EJE.png";
+import image from "./EJE.jpg";
+import slide1 from "./Slider/1.png"
+import slide2 from "./Slider/2.jpg"
+import slide3 from "./Slider/3.jpg"
+import slide4 from "./Slider/4.jpg"
+import slide5 from "./Slider/5.jpg"
+import banner from "./photoo.jpg"
+import logo from "./photoo.jpg"
+
+const Slider_list=[slide1,slide2,slide3,slide4,slide5]
+
+
+export { bannerrImg };
+export { downloadIcon };
+export { mailIcon };
+export {bannerImg, image};
+export {Slider_list}
+export {banner}
+export {logo}
