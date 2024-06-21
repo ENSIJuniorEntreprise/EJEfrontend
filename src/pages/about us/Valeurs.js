3f2b2d136437e9b26a290ff91b9d18af9b86b88b
ENSI Junior Entreprise <148913750+ENSIJuniorEntreprise@users.noreply.github.com>
2024-06-21 21:24:54 +0100

    Add files via upload

---
+++ src/pages/about us/Valeurs.js
@@ -0,0 +1,40 @@
+import creativeIcon from './img/creative1.png';
+import excellenceIcon from './img/excellence1.png';
+import tieIcon from './img/tie1.png';
+const Valeurs = [
+  {
+    backgroundColor: "#E0DED2",
+    iconBackground: "#E0DED2",
+    iconBackgroundImage: creativeIcon,
+    title: "Creativity",
+    lineColor: "#2DA2DD",
+    textColor: "#1F2029",
+    description: "Unlock creativity and innovation with ENSI Junior Enterprise.",
+    marginTop: " xxs:mt-[30px] md:mt-[100px] dmd:mt-[140px] dlg:mt-[180px]",
+    marginBottom: "mb-6"
+  },
+  {
+    backgroundColor: "#2DA2DD",
+    iconBackground: "#2DA2DD",
+    iconBackgroundImage: excellenceIcon,
+    title: "Excellence",
+    lineColor: "#1F2029",
+    textColor: "#E0DED2",
+    description: "Reach new heights of excellence with ENSI Junior Enterprise.",
+    marginTop: " md:mt-[20px]  dmd:mt-[40px] dlg:mt-[60px]",
+    marginBottom: "mb-6"
+  },
+  {
+    backgroundColor: "#E0DED2",
+    iconBackground: "#E0DED2",
+    iconBackgroundImage: tieIcon,
+    title: "Professionalism",
+    lineColor: "#2DA2DD",
+    textColor: "#1F2029",
+    description: "Discover the professionalism that sets ENSI Junior Enterprise apart.",
+    marginTop: "md:mt-[100px] dmd:mt-[140px] dlg:mt-[180px]",
+    marginBottom: "mb-6"
+  }
+];
+
+export default Valeurs;
